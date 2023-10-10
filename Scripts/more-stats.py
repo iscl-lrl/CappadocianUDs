3e75ec3d0687318aefff79efc5cf2c96cd40db59
Cagri Coltekin <cagri@coltekin.net>
2023-10-10 11:55:59 +0200

    Minor changes to statistics.

--- Scripts/more-stats.py
+++ Scripts/more-stats.py
@@ -2,6 +2,7 @@
 
 #import numpy as np
 from conllu import conllu_sentences
+from collections import Counter
 
 infile = 'AnnotationsFinal/AnnotationsFinal.conllu'
 
@@ -11,15 +12,22 @@ feats = []
 multi = 0
 for sent in conllu_sentences(infile):
     sentlen.append(len(sent))
+    multi += len(sent.multi)
     for tok in sent.nodes[1:]:
         postags.append(tok.upos)
         if tok.feats and tok.feats != '_':
             tokfeats = tok.feats.split('|')
             if tokfeats:
-                feats.extend([f.split('=') for f in tokfeats])
-        print(tok.form, tok.multi)
+                feats.extend([tuple([tok.upos] + f.split('=')) for f in tokfeats])
 
 print(f"Sentences: {len(sentlen)}")
 print(f"Tokens: {len(postags)}")
+nfeatval = len(set(feats))
+nfeat = len(set([fv[0] for fv in feats]))
 print(f"Avglen (tokens): {sum(sentlen)/len(sentlen)}")
 print(f"Avglen (words): {(sum(sentlen)-multi)/len(sentlen)}")
+
+#print(f"Features: ", Counter([(fv[0], fv[1]) for fv in feats]).most_common())
+for p, f in sorted(set([(fv[0], fv[1]) for fv in feats])):
+    print(p, f)
+print(f"Features/values: ", Counter(feats).most_common())
